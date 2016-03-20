daef754f1c32636204e87403a62fe9e5581be52e
Jason Kong <jasonkongnz@gmail.com>
2016-03-21 08:49:51 +1300

    Adding service interface

---
+++ TimeCapture.Service/IRecordingService.cs
@@ -0,0 +1,11 @@
+﻿using TimeCapture.Models;
+
+namespace TimeCapture.Service
+{
+    interface IRecordingService
+    {
+        void InsertRecording(RecordViewModel model);
+        void UpdateRecording(RecordViewModel model);
+        void DeleteRecording(RecordViewModel model);
+    }
+}
---
+++ TimeCapture.Service/RecordingService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeCapture.Models;
+
+namespace TimeCapture.Service
+{
+    public class RecordingService : IRecordingService
+    {
+        public void DeleteRecording(RecordViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertRecording(RecordViewModel model)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public void UpdateRecording(RecordViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
